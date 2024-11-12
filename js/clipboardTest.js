f4c1e8c6e318a572efdc3731b9c779be89b6c38d
miembrodes-work <138647317+miembrodes-work@users.noreply.github.com>
2024-11-12 12:02:14 +0900

    Update clipboardTest.js

--- js/clipboardTest.js
+++ js/clipboardTest.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  $("#thisB").on("click", function(){
-    alert("hello");
+  $("#thisB").on("click", function(e){
+    //alert("hello");
+
+    var cliptest = "adsfasdfasdf";
+    e.clipboardData.setData("text/plain", cliptest);
+    e.preventDefault();
+    
   });
 });
 
