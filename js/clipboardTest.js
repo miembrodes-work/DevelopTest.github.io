3e3932156ce9b4f2035fb94a9e0ef160c422f3c1
miembrodes-work <138647317+miembrodes-work@users.noreply.github.com>
2024-11-12 12:05:59 +0900

    Update clipboardTest.js

--- js/clipboardTest.js
+++ js/clipboardTest.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#thisB").on("click", function(e){
+    
     //alert("hello");
-
     var cliptest = "adsfasdfasdf";
     e.clipboardData.setData("text/plain", cliptest);
     e.preventDefault();
